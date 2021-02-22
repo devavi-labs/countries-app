0903d2a89c79220f286085eef47cfc6e6ef23e2c
Avinash Sinha <savinash2608@gmail.com>
2021-02-23 04:50:51 +0530

    material-ui styling warning fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,24 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
-import { Provider } from "react-redux";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import { AppProps } from "next/app";
-import "../styles/globals.css";
+import React from "react";
+import { Provider } from "react-redux";
 import { useStore } from "../global/store";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const store = useStore(pageProps.initialReduxState);
 
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
+      <CssBaseline />
       <Component {...pageProps} />
     </Provider>
   );
