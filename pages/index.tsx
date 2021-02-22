8f4a2e104dde8d561d6cfc5073d9dcbe566955e9
Avinash Sinha <savinash2608@gmail.com>
2021-02-23 00:14:44 +0530

    material ui added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,45 @@
+import AppBar from "@material-ui/core/AppBar";
+import Container from "@material-ui/core/Container";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
+import { makeStyles } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/home.module.css";
+import React from "react";
 
-export default function Home() {
+const Home: NextPage = () => {
+  const useStyles = makeStyles<Theme>(({ breakpoints: { width } }) => ({
+    toolbar: {
+      width: "100%",
+      maxWidth: width("lg"),
+      margin: "0 auto",
+    },
+    title: {
+      fontWeight: "bold",
+    },
+  }));
+  const classes = useStyles();
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Countries App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1>The Countries App</h1>
+      <main>
+        <AppBar position="sticky" elevation={0}>
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h5" component="span" className={classes.title}>
+              Countries
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="lg"></Container>
       </main>
     </div>
   );
-}
+};
+
+export default Home;
