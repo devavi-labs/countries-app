c0b37da7b3e1377e4e2cf9860a6da306cb90bdaa
Avinash Sinha <savinash2608@gmail.com>
2021-02-24 00:39:57 +0530

    flag shadow fixed

--- styles/flag.ts
+++ styles/flag.ts
@@ -1,12 +1,12 @@
 import { alpha, makeStyles, createStyles } from "@material-ui/core/styles";
 
-export const useStyles = makeStyles(({ palette: { common } }) =>
+export const useStyles = makeStyles(({ shadows }) =>
   createStyles({
     flag: {
       width: 33,
       height: 22.5,
       objectFit: "cover",
-      boxShadow: `0 0 8px 2px ${alpha(common.black, 0.3)}`,
+      boxShadow: shadows[10],
     },
   })
 );
