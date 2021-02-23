deb89fb904a32a2adda4ebb916490fa502296c06
Avinash Sinha <savinash2608@gmail.com>
2021-02-24 00:38:44 +0530

    home styling improved

--- styles/home.ts
+++ styles/home.ts
@@ -1,23 +1,27 @@
 import { makeStyles } from "@material-ui/core/styles";
 import { Theme } from "@material-ui/core/styles/createMuiTheme";
 
-const useStyles = makeStyles<Theme>(({ breakpoints: { width, up } }) => ({
-  toolbar: {
-    width: "100%",
-    maxWidth: width("lg"),
-    margin: "0 auto",
-  },
-  title: {
-    flexGrow: 1,
-    display: "none",
-    [up("sm")]: {
-      display: "block",
-      fontWeight: "bold",
+const useStyles = makeStyles<Theme>(
+  ({ breakpoints: { width, up }, spacing }) => ({
+    toolbar: {
+      width: "100%",
+      maxWidth: width("lg"),
+      margin: "0 auto",
+      height: spacing(6),
     },
-  },
-  container: {
-    height: "calc(100vh - 80px)",
-  },
-}));
+    title: {
+      flexGrow: 1,
+      display: "none",
+      [up("sm")]: {
+        display: "block",
+        fontWeight: "bold",
+      },
+    },
+    container: {
+      height: `calc(100vh - ${spacing(10)})`,
+      margin: spacing(1, "auto"),
+    },
+  })
+);
 
 export default useStyles;
